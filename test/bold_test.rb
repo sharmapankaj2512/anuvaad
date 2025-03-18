c5e8952d42128916ceeb637f1874ac82c6d458d1
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-18 20:24:20 +0530

    Multiple bold occurences

--- test/bold_test.rb
+++ test/bold_test.rb
@@ -6,4 +6,9 @@ class BoldTest < Minitest::Test
   def test_single_bold_occurrence
     assert_equal Markdown.new("**ultimate text**").to_html, "<b>ultimate text</b>"
   end
+
+  def test_multiple_bold_occurrences
+    markdown = Markdown.new("The **ultimate text** is **here**.")
+    assert_equal markdown.to_html, "The <b>ultimate text</b> is <b>here</b>."
+  end
 end
