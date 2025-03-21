9186436dca2481b68b7f8f6ade206130e4f70047
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-21 13:46:31 +0530

    Refactor extract class

--- src/markdown.rb
+++ src/markdown.rb
@@ -28,8 +28,11 @@ class Markdown
         html << content
         index += lines_processed
       elsif contains_italic_marker(line)
-        html << italic_markers_to_html(line)
-        index += 1
+        raw_line = RawLine.new(line)
+        mark_down = raw_line.to_markdown(lines, index)
+        content, lines_processed = mark_down.to_html
+        html << content
+        index += lines_processed
       elsif contains_unordered_list_marker(line)
         list_items, index = unordered_list_markers(index, line, lines)
         html << unordered_list_items_to_html(list_items)
@@ -79,6 +82,7 @@ class RawLine
 
   def to_markdown(lines, current_line_index)
     return BoldMarker.new(@line, lines, current_line_index) if contains_bold_marker(@line)
+    return ItalicMarker.new(@line, lines, current_line_index) if contains_italic_marker(@line)
 
     HeadingMarker.new(@line, lines, current_line_index) if is_heading_marker
   end
@@ -89,6 +93,11 @@ class RawLine
 
   def contains_bold_marker(line)
     pattern = /(\*\*.*?\*\*)|(__.*?__)/
+    line.match?(pattern)
+  end
+
+  def contains_italic_marker(line)
+    pattern = /(\*[^*]+\*)|(_[^_]+_)/
     line.match?(pattern)
   end
 end
@@ -137,5 +146,21 @@ class BoldMarker
 
   def bold_tags
     @line.gsub(/(\*\*(.*?)\*\*)|(__([^_]+)__)/, '<b>\2\4</b>')
+  end
+end
+
+class ItalicMarker
+  def initialize(line, lines, current_line_index)
+    @line = line
+    @lines = lines
+    @index = current_line_index
+  end
+
+  def to_html
+    [italic_tags, 1]
+  end
+
+  def italic_tags
+    @line.gsub(/(\*(.*?)\*)|(_(.*?)_)/, '<i>\2\4</i>')
   end
 end
