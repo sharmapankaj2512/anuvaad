f7afcaadab1d9bf1484c79a4ee6ebd2f025afdd6
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-21 14:07:27 +0530

    Refactor extract class

--- src/markdown.rb
+++ src/markdown.rb
@@ -34,8 +34,11 @@ class Markdown
         html << content
         index += lines_processed
       elsif contains_unordered_list_marker(line)
-        list_items, index = unordered_list_markers(index, line, lines)
-        html << unordered_list_items_to_html(list_items)
+        raw_line = RawLine.new(line)
+        mark_down = raw_line.to_markdown(lines, index)
+        content, lines_processed = mark_down.to_html
+        html << content
+        index += lines_processed
       end
     end
     html
@@ -83,6 +86,7 @@ class RawLine
   def to_markdown(lines, current_line_index)
     return BoldMarker.new(@line, lines, current_line_index) if contains_bold_marker(@line)
     return ItalicMarker.new(@line, lines, current_line_index) if contains_italic_marker(@line)
+    return UnorderedListMarker.new(@line, lines, current_line_index) if contains_unordered_list_marker(@line)
 
     HeadingMarker.new(@line, lines, current_line_index) if is_heading_marker
   end
@@ -99,6 +103,12 @@ class RawLine
   def contains_italic_marker(line)
     pattern = /(\*[^*]+\*)|(_[^_]+_)/
     line.match?(pattern)
+  end
+
+  def contains_unordered_list_marker(line)
+    return false if line.nil?
+
+    line.start_with?('-')
   end
 end
 
@@ -162,5 +172,52 @@ class ItalicMarker
 
   def italic_tags
     @line.gsub(/(\*(.*?)\*)|(_(.*?)_)/, '<i>\2\4</i>')
+  end
+end
+
+class UnorderedListMarker
+  include Text
+
+  def initialize(line, lines, current_line_index)
+    @line = line
+    @lines = lines
+    @index = current_line_index
+  end
+
+  def to_html
+    list_items, index = unordered_list_markers(@index, @line, @lines)
+    [unordered_list_items_to_html(list_items), index]
+  end
+
+  def unordered_list_markers(start_index, line, lines)
+    index = start_index
+    list_items = []
+    while contains_unordered_list_marker(line)
+      list_items << line
+      index += 1
+      line = lines[index]
+    end
+    [list_items, index]
+  end
+
+  def unordered_list_items_to_html(list_items)
+    html = String.new('<ul>')
+    list_items.each do |list_item|
+      html << unordered_list_marker_to_html(list_item)
+    end
+    html << '</ul>'
+    html
+  end
+
+  def unordered_list_marker_to_html(line)
+    marker = line[/^-+(?=\s)/]
+    inline_text = text_after(marker, line)
+    "<li>#{inline_text}</li>"
+  end
+
+  def contains_unordered_list_marker(line)
+    return false if line.nil?
+
+    line.start_with?('-')
   end
 end
