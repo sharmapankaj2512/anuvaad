f24655f6d26f3b0898a4608cb5520fef9eee446e
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-21 16:52:01 +0530

    Refactor rename tests for clarity

--- test/lists_test.rb
+++ test/lists_test.rb
@@ -3,11 +3,11 @@
 require 'minitest/autorun'
 
 class ListsTest < Minitest::Test
-  def test_single_list_item
+  def test_single_unordered_list_item
     assert_equal Markdown.new('- Item 1').to_html, '<ul><li>Item 1</li></ul>'
   end
 
-  def test_multiple_list_items
+  def test_multiple_unordered_list_items
     assert_equal Markdown.new("- Item 1\n- Item 2").to_html, '<ul><li>Item 1</li><li>Item 2</li></ul>'
   end
 end
