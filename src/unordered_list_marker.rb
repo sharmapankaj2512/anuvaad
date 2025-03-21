44a6b5bc1d082988835204e0b39f25440972bd0e
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-21 14:30:21 +0530

    Refactor move to file

---
+++ src/unordered_list_marker.rb
@@ -0,0 +1,46 @@
+class UnorderedListMarker
+  include Text
+
+  def initialize(line, lines, current_line_index)
+    @line = line
+    @lines = lines
+    @index = current_line_index
+  end
+
+  def to_html
+    list_items, index = unordered_list_markers(@index, @line, @lines)
+    [unordered_list_items_to_html(list_items), index]
+  end
+
+  def unordered_list_markers(start_index, line, lines)
+    index = start_index
+    list_items = []
+    while UnorderedListMarker.contains_unordered_list_marker(line)
+      list_items << line
+      index += 1
+      line = lines[index]
+    end
+    [list_items, index]
+  end
+
+  def unordered_list_items_to_html(list_items)
+    html = String.new('<ul>')
+    list_items.each do |list_item|
+      html << unordered_list_marker_to_html(list_item)
+    end
+    html << '</ul>'
+    html
+  end
+
+  def unordered_list_marker_to_html(line)
+    marker = line[/^-+(?=\s)/]
+    inline_text = text_after(marker, line)
+    "<li>#{inline_text}</li>"
+  end
+
+  def self.contains_unordered_list_marker(line)
+    return false if line.nil?
+
+    line.start_with?('-')
+  end
+end
