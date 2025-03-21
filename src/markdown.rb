8605520a6b3e8bebae4d7f85411ac02fc13a0e45
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-21 14:30:06 +0530

    Refactor move to file

--- src/markdown.rb
+++ src/markdown.rb
@@ -31,26 +31,7 @@ require_relative 'heading_marker.rb'
 
 require_relative 'bold_marker.rb'
 
-class ItalicMarker
-  def initialize(line, lines, current_line_index)
-    @line = line
-    @lines = lines
-    @index = current_line_index
-  end
-
-  def self.contains_italic_marker(line)
-    pattern = /(\*[^*]+\*)|(_[^_]+_)/
-    line.match?(pattern)
-  end
-
-  def to_html
-    [italic_tags, 1]
-  end
-
-  def italic_tags
-    @line.gsub(/(\*(.*?)\*)|(_(.*?)_)/, '<i>\2\4</i>')
-  end
-end
+require_relative 'italic_marker.rb'
 
 class UnorderedListMarker
   include Text
