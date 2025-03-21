a07b9a578cdb25efed36913ab68b749bf957cfc4
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-21 17:51:54 +0530

    Multiple ordered list

--- test/lists_test.rb
+++ test/lists_test.rb
@@ -15,4 +15,8 @@ class ListsTest < Minitest::Test
   def test_single_ordered_list_item
     assert_equal Markdown.new('1. Item 1').to_html, '<ol><li>Item 1</li></ol>'
   end
+
+  def test_multiple_ordered_list_items
+    assert_equal Markdown.new("1. Item 1\n2. Item 2").to_html, '<ol><li>Item 1</li><li>Item 2</li></ol>'
+  end
 end
