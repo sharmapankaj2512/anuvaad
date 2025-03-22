6d11983c6343c2d073a97b8c09ca11fdb7b72020
Pankaj Sharma <sharmapankaj2512@gmail.com>
2025-03-22 10:28:51 +0530

    Revert "Refactor rename file"
    This reverts commit e191f710b4438e4bf3a21cb4b7aa15ebeedd9e6c.

--- src/list_marker.rb
+++ src/list_marker.rb
@@ -1,21 +1,25 @@
+require_relative 'text'
+
 class ListMarker
   include Text
 
-  def initialize(line, lines, current_line_index)
+  def initialize(line, lines, current_line_index, start_tag, end_tag)
     @line = line
     @lines = lines
     @index = current_line_index
+    @start_tag = start_tag
+    @end_tag = end_tag
   end
 
   def to_html
-    list_items, index = list_markers(@index, @line, @lines)
-    [list_items_to_html(list_items), index]
+    marked_list_items, lines_processed = list_markers(@index, @line, @lines)
+    [list_items_to_html(marked_list_items), lines_processed]
   end
 
   def list_markers(start_index, line, lines)
     index = start_index
     list_items = []
-    while ListMarker.is_present(line)
+    while self.class.is_present(line)
       list_items << line
       index += 1
       line = lines[index]
@@ -24,23 +28,53 @@ class ListMarker
   end
 
   def list_items_to_html(list_items)
-    html = String.new('<ul>')
+    html = String.new(@start_tag)
     list_items.each do |list_item|
       html << list_marker_to_html(list_item)
     end
-    html << '</ul>'
+    html << @end_tag
     html
   end
 
   def list_marker_to_html(line)
-    marker = line[/^-+(?=\s)/]
+    marker = marker_pattern(line)
     inline_text = text_after(marker, line)
     "<li>#{inline_text}</li>"
+  end
+
+  private
+
+  def marker_pattern(line)
+    line[/^-+(?=\s)/]
+  end
+end
+
+class UnorderedListItems < ListMarker
+  def initialize(line, lines, current_line_index)
+    super(line, lines, current_line_index, '<ul>', '</ul>')
   end
 
   def self.is_present(line)
     return false if line.nil?
 
     line.start_with?('-')
+  end
+end
+
+class OrderedListItems < ListMarker
+  def initialize(line, lines, current_line_index)
+    super(line, lines, current_line_index, '<ol>', '</ol>')
+  end
+
+  def self.is_present(line)
+    return false if line.nil?
+
+    line =~ /^\d+\.\s/
+  end
+
+  private
+
+  def marker_pattern(line)
+    line[/^\d+\.(?=\s)/]
   end
 end
---
+++ src/unordered_list_marker.rb
@@ -0,0 +1,46 @@
+class ListMarker
+  include Text
+
+  def initialize(line, lines, current_line_index)
+    @line = line
+    @lines = lines
+    @index = current_line_index
+  end
+
+  def to_html
+    list_items, index = list_markers(@index, @line, @lines)
+    [list_items_to_html(list_items), index]
+  end
+
+  def list_markers(start_index, line, lines)
+    index = start_index
+    list_items = []
+    while ListMarker.is_present(line)
+      list_items << line
+      index += 1
+      line = lines[index]
+    end
+    [list_items, index]
+  end
+
+  def list_items_to_html(list_items)
+    html = String.new('<ul>')
+    list_items.each do |list_item|
+      html << list_marker_to_html(list_item)
+    end
+    html << '</ul>'
+    html
+  end
+
+  def list_marker_to_html(line)
+    marker = line[/^-+(?=\s)/]
+    inline_text = text_after(marker, line)
+    "<li>#{inline_text}</li>"
+  end
+
+  def self.is_present(line)
+    return false if line.nil?
+
+    line.start_with?('-')
+  end
+end
